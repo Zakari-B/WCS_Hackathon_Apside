e1c67ac64d78271d7edf2014e64d3d21aeccdc63
Boureghda Zakari <zakari.boureghda@gmail.com>
2022-06-29 20:55:53 +0200

    Added admin tools and user creation for admins

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const UserController = require("./controllers/UserController");
 
-const { authorization, sessionControl } = require("./middlewares/auth");
+const {
+  authorization,
+  authorizeAdmin,
+  sessionControl,
+} = require("./middlewares/auth");
 
 const router = express.Router();
 
 router.post("/auth/login", UserController.login);
 router.get("/auth/logout", authorization, UserController.logout);
 router.get("/auth/sessionControl", authorization, sessionControl);
+
+router.post(
+  "/admin/create",
+  authorization,
+  authorizeAdmin,
+  UserController.createOne
+);
 
 module.exports = router;
