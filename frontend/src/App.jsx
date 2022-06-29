ba65e1b05e6e91e3b5a008fdda68e64a90f43e8e
gnos28 <gnos28@gmail.com>
2022-06-29 17:43:03 +0200

    bubble working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,34 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import * as d3 from "d3";
+import ClusteredBubbles from "./components/ClusteredBubbles";
 import "./App.css";
+
+const dimensions = {
+  width: 600,
+  height: 600,
+  margin: { top: 30, right: 30, bottom: 30, left: 60 },
+};
+const n = 50; // number of nodes
+const m = 10; // number of groups
+const data = {
+  children: Array.from(
+    d3.group(
+      Array.from({ length: n }, (_, i) => ({
+        group: Math.random() * m || 0,
+        value: -Math.log(Math.random()),
+      })),
+      (d) => d.group
+    ),
+    ([, children]) => ({ children })
+  ),
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <ClusteredBubbles data={data} dimensions={dimensions} />
       </header>
     </div>
   );
